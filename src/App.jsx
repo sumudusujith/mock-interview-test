2bf942877e397ad64e3cc7b147df7e69a552ab0e
Maddumage <uomroshan@gmail.com>
2024-06-08 09:27:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const data = [23, 12, 45, 66, 34, 98, 35, 52, 71, 18];
+  return (
+    <>
+      <h2>Mock Interview 1 - Test</h2>
+      <div className="card">
+        <p>
+          Input is <code>{data.toString()}</code>
+        </p>
+        <button>Generate Result</button>
+        <p>
+          Output is <code>{[12, 34].toString()}</code>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
